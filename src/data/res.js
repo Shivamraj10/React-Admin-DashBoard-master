1d5cca164b2031f7be8b479e55fd0620934367f9
Shivam Raj <shivamrajpoddar01@gmail.com>
2025-06-07 12:44:35 +0530

    Add full React Admin Dashboard project

---
+++ src/data/res.js
@@ -0,0 +1,55 @@
+export const response = {
+  data: [
+    {
+      title: "Revenues",
+      value: 35289900,
+    },
+    {
+      title: "Transactions",
+      value: 450,
+    },
+    {
+      title: "Likes",
+      value: 15810,
+    },
+    {
+      title: "Users",
+      value: 1290,
+    },
+  ],
+  chartData: {
+    User: {
+      Week0: 100,
+      Week1: 400,
+      Week2: 200,
+      Week3: 500,
+      Week4: 250,
+      Week5: 367,
+    },
+    Guest: {
+      Week0: 200,
+      Week1: 400,
+      Week2: 200,
+      Week3: 500,
+      Week4: 700,
+      Week5: 267,
+    },
+  },
+  pieChartData: {
+    "Software Solutions": 45,
+    "Cloud Services": 25,
+    "IT Consultancy": 30,
+  },
+  todaysSchedule: [
+    {
+      time: "09:00-11:00",
+      location: "Delhi, India",
+      meetingTitle: "Client Presentation for New Web Project",
+    },
+    {
+      time: "14:00-15:00",
+      location: "Hyderabad, Telangana",
+      meetingTitle: "Discussion on Agile Implementation",
+    },
+  ],
+};
